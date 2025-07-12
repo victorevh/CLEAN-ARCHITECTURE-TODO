63856fe4c3bf1d90f7f81e6b38a2d23c62b6f18d
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-07-12 18:08:32 -0300

    chore: create get

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
