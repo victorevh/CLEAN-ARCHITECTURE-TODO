c53431bd3e02449bcd8be9187854263f4cd8e120
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-07-13 01:21:04 -0300

    feat(http-routes): implements list task

--- src/interface/http/routes/task-routes.ts
+++ src/interface/http/routes/task-routes.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
-import { adaptRoute } from "@interface/http/adapters/express-adapter";
-import { CreateTaskController } from "@interface/http/controllers/CreateTaskController";
 import container from "@core/container";
 import TYPES from "@core/types";
+import { adaptRoute } from "@interface/http/adapters/express-adapter";
+import { CreateTaskController } from "@interface/http/controllers/CreateTaskController";
+import { ListTasksController } from "@interface/http/controllers/ListTasksController";
 
 const router = Router();
 
 const createTaskController = container.get<CreateTaskController>(
   TYPES.CreateTaskController
 );
+const listTasksController = container.get<ListTasksController>(
+  TYPES.ListTasksUseCase
+);
 
 router.post("/tasks", adaptRoute(createTaskController));
+router.get("/tasks", adaptRoute(listTasksController));
 
 export default router;
