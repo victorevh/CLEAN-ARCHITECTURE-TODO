b7a00122193e74f2c31224b2863dec1d20e8508f
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-07-13 10:53:08 -0300

    feat(interface): add get task by id route

--- src/interface/http/routes/task-routes.ts
+++ src/interface/http/routes/task-routes.ts
@@ -3,6 +3,7 @@ import { adaptRoute } from "@interface/http/adapters/express-adapter";
 import { CreateTaskController } from "@interface/http/controllers/CreateTaskController";
 import { ListTasksController } from "@interface/http/controllers/ListTasksController";
 import { UpdateTaskController } from "@interface/http/controllers/UpdateTaskController";
+import { GetTaskByIdController } from "@interface/http/controllers/GetTaskByIdController";
 import container from "@core/container";
 import TYPES from "@core/types";
 
@@ -17,9 +18,13 @@ const listTasksController = container.get<ListTasksController>(
 const updateTaskController = container.get<UpdateTaskController>(
   TYPES.UpdateTaskController
 );
+const getTaskByIdController = container.get<GetTaskByIdController>(
+  TYPES.GetTaskByIdController
+);
 
 router.post("/tasks", adaptRoute(createTaskController));
 router.get("/tasks", adaptRoute(listTasksController));
 router.patch("/tasks/:id", adaptRoute(updateTaskController));
+router.get("/tasks/:id", adaptRoute(getTaskByIdController));
 
 export default router;
