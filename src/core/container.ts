ce72aaa121ce1bc399a026ed3ac7325329f15405
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-07-12 23:17:58 -0300

    feat(core): implement Inversify container

---
+++ src/core/container.ts
@@ -0,0 +1,36 @@
+import { Container } from "inversify";
+import TYPES from "./types";
+import type { ITaskRepository } from "@domain/repositories/ITaskRepository";
+import { InMemoryTaskRepository } from "@infra/repositories/InMemoryTaskRepository";
+import { CreateTaskUseCase } from "@application/use-cases/create-task/CreateTaskUseCase";
+import { ListTasksUseCase } from "@application/use-cases/list-tasks/ListTasksUseCase";
+import { GetTaskByIdUseCase } from "@application/use-cases/get-task-by-id/GetTaskByIdUseCase";
+import { UpdateTaskUseCase } from "@application/use-cases/update-task/UpdateTaskUseCase";
+import { DeleteTaskUseCase } from "@application/use-cases/delete-task/DeleteTaskUseCase";
+import { CompleteTaskUseCase } from "@application/use-cases/complete-task/CompleteTaskUseCase";
+
+const container = new Container();
+
+container
+  .bind<ITaskRepository>(TYPES.ITaskRepository)
+  .to(InMemoryTaskRepository)
+  .inSingletonScope();
+
+container
+  .bind<CreateTaskUseCase>(TYPES.CreateTaskUseCase)
+  .to(CreateTaskUseCase);
+container.bind<ListTasksUseCase>(TYPES.ListTasksUseCase).to(ListTasksUseCase);
+container
+  .bind<GetTaskByIdUseCase>(TYPES.GetTaskByIdUseCase)
+  .to(GetTaskByIdUseCase);
+container
+  .bind<UpdateTaskUseCase>(TYPES.UpdateTaskUseCase)
+  .to(UpdateTaskUseCase);
+container
+  .bind<DeleteTaskUseCase>(TYPES.DeleteTaskUseCase)
+  .to(DeleteTaskUseCase);
+container
+  .bind<CompleteTaskUseCase>(TYPES.CompleteTaskUseCase)
+  .to(CompleteTaskUseCase);
+
+export default container;
