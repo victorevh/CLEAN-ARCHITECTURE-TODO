60d54a2a3717ee2718e2f4f04da0af0e48d40510
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-07-13 00:35:48 -0300

    fix(http): get container of controller

--- src/interface/http/routes/task-routes.ts
+++ src/interface/http/routes/task-routes.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import { adaptRoute } from "@interface/http/adapters/express-adapter";
 import { CreateTaskController } from "@interface/http/controllers/CreateTaskController";
 import container from "@core/container";
+import TYPES from "@core/types";
 
 const router = Router();
 
-const createTaskController = container.get(CreateTaskController);
+const createTaskController = container.get<CreateTaskController>(
+  TYPES.CreateTaskController
+);
+
 router.post("/tasks", adaptRoute(createTaskController));
 
 export default router;
