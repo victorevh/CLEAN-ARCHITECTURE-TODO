005b31bcda2a9d2a2899eeb39f3704e57ed13c5c
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-07-13 01:26:36 -0300

    feat(container): add list task controller type

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,12 +1,16 @@
 const TYPES = {
+  // Repositories
   ITaskRepository: Symbol.for("ITaskRepository"),
+  // Use Cases
   CreateTaskUseCase: Symbol.for("CreateTaskUseCase"),
   ListTasksUseCase: Symbol.for("ListTasksUseCase"),
   UpdateTaskUseCase: Symbol.for("UpdateTaskUseCase"),
   DeleteTaskUseCase: Symbol.for("DeleteTaskUseCase"),
   CompleteTaskUseCase: Symbol.for("CompleteTaskUseCase"),
   GetTaskByIdUseCase: Symbol.for("GetTaskByIdUseCase"),
+  // Controllers
   CreateTaskController: Symbol.for("CreateTaskController"),
+  ListTasksController: Symbol.for("ListTasksController"),
 };
 
 export default TYPES;
