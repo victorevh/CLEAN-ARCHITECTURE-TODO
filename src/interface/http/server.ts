711e637a4ff0b2cd9b91311ff7c48ac5e7b8324a
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2025-07-13 13:03:14 -0300

    chore(dotenv): implements dotenv config

--- src/interface/http/server.ts
+++ src/interface/http/server.ts
@@ -1,12 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import "reflect-metadata";
 import express from "express";
 import taskRoutes from "@interface/http/routes/task-routes";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", taskRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
